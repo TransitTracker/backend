5654d279816449f02dbee52a4c1fb8ae44f26d7c
Félix Desjardins <felixinx@gmail.com>
2020-07-30 09:59:09 -0400

    Bump version to 2.3.0 on Tab Home

--- resources/js/components/TabHome.vue
+++ resources/js/components/TabHome.vue
@@ -7,7 +7,7 @@
             {{ $vuetify.lang.t('$vuetify.home.welcome') }} Transit&nbsp;Tracker
           </h1>
           <h2 class="text-subtitle-1">
-            {{ $vuetify.lang.t('$vuetify.home.version') }} 2.2.0
+            {{ $vuetify.lang.t('$vuetify.home.version') }} 2.3.0
           </h2>
           <v-btn
             icon

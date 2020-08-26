6a62c1fbfbe673abdafc870015cbff4b66a13351
Félix Desjardins <felixinx@gmail.com>
2020-08-25 20:12:05 -0400

    Added Bugsnag to frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,11 +16,23 @@ import Vue from 'vue'
 
 // Matomo
 import VueMatomo from 'vue-matomo'
+
+// Bugsnag
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginVue from '@bugsnag/plugin-vue'
+
 Vue.use(VueMatomo, {
   host: process.env.MIX_MATOMO_HOST,
   siteId: process.env.MIX_MATOMO_SITE_ID,
   router: router
 })
+
+Bugsnag.start({
+  apiKey: process.env.MIX_BUGNSAG_API_KEY,
+  plugins: [new BugsnagPluginVue()],
+})
+Bugsnag.getPlugin('vue')
+  .installVueErrorHandler(Vue)
 
 // Vue comes to life!
 window.vm = new Vue({

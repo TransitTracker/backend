61f3d4535c833cd3f509a9272a7be685012b7cfe
Félix Desjardins <FelixINX@users.noreply.github.com>
2021-04-28 12:20:53 -0400

    :ambulance: Hot fix
    For exo VIN problem

--- app/Jobs/RefreshForGTFS.php
+++ app/Jobs/RefreshForGTFS.php
@@ -118,7 +118,7 @@ class RefreshForGTFS implements ShouldQueue
 
             // vehicle->label
             // Don't use the label feed for GO Transit
-            if ($vehicle->getVehicle()->getLabel() && (!in_array($this->agency->slug, ['go']))) {
+            if ($vehicle->getVehicle()->getLabel() && (!in_array($this->agency->slug, ['go', 'la', 'vr', 'lr', 'lasso', 'sju', 'so', 'hsl', 'pi', 'rous', 'sv', 'tm', 'crc']))) {
                 $newVehicle['label'] = $vehicle->getVehicle()->getLabel();
             } else {
                 $newVehicle['label'] = null;

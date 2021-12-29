c870e76145dd9ed25045e291ecbcf3afcc9cf200
Félix Desjardins <felixinx@gmail.com>
2021-12-29 16:46:27 -0500

    💄 Material 3 for VIN show

--- resources/views/vin/show.blade.php
+++ resources/views/vin/show.blade.php
@@ -7,12 +7,12 @@
         <div class="md:col-span-2">
             <ul class="flex overflow-auto gap-x-4">
                 @foreach($vehicles as $vehicle)
-                    <li class="flex-shrink-0 px-4 py-2 mb-2 space-y-2 text-sm bg-white rounded dark:bg-m3-surface-dark-variant dark:text-m3-surface-dark-on-variant">
-                        <div class="inline px-2 py-1 rounded" style="color: {{ $vehicle->agency->text_color }};background-color: {{ $vehicle->agency->color }};">
+                    <li class="flex-shrink-0 p-2 mb-2 space-y-2 rounded bg-m3-surface-variant text-m3-surface-on-variant dark:bg-m3-surface-dark-variant dark:text-m3-surface-dark-on-variant">
+                        <div class="inline px-2 py-1 text-sm rounded" style="color: {{ $vehicle->agency->text_color }};background-color: {{ $vehicle->agency->color }};">
                             {{ $vehicle->agency->short_name }}
                         </div>
-                        @if($vehicle->force_label)<small class="ml-2 text-xs tracking-wide">{{ __('Fleet label:') }} {{ $vehicle->force_label }}</small>@endif
-                        <div>{{ __('Last seen:') }} {{ now()->parse($vehicle->updated_at)->diffForHumans() }}</div>
+                        @if($vehicle->force_label)<span class="ml-2 font-medium leading-6">{{ $vehicle->force_label }}</span>@endif
+                        <div class="text-xs font-medium leading-4">{{ __('Last seen:') }} {{ now()->parse($vehicle->updated_at)->diffForHumans() }}</div>
                     </li>
                 @endforeach
             </ul>
@@ -23,11 +23,11 @@
                 <x-gmdi-check class="w-12 h-12 text-green-700 fill-current" />
             </div>
             @else
-            <form action="" method="POST" class="p-4 mt-6 bg-white rounded shadow dark:bg-m3-surface-dark dark:text-m3-surface-dark-on dark:border dark:border-m3-background-dark-outline" id="form-suggest">
+            <form action="" method="POST" class="p-4 mt-6 border rounded-2xl text-m3-surface-on dark:bg-m3-surface-dark dark:text-m3-surface-dark-on border-m3-background-outline dark:border-m3-background-dark-outline" id="form-suggest">
                 @csrf
                 <input type="hidden" value="{{ $vin }}" id="vin" name="vin">
                 
-                <h2 class="text-xl font-medium leading-8 tracking-wide">{{ __('Submit a new fleet number') }}</h2>
+                <h2 class="text-[1.375rem] leading-8">{{ __('Submit a new fleet number') }}</h2>
 
                 <div class="relative mt-8 mb-4">
                     <input type="text" id="label" placeholder="{{ __('Fleet number') }}" name="label" required
@@ -55,7 +55,7 @@
                 <div class="mb-4 text-xs font-medium text-red-500">{{ __('The reCAPTCHA validation has failed. Please try again.') }}</div>
                 @enderror
 
-                <div class="flex gap-x-2">
+                <div class="flex">
                     <button data-sitekey="{{ config('transittracker.recaptcha.site') }}" data-callback='onSuggestSubmit'
                         class="relative flex items-center h-10 px-6 text-sm font-medium transition-colors rounded-full bg-m3-primary text-m3-primary-on dark:bg-m3-primary-dark dark:text-m3-primary-dark-on hover:bg-opacity-85 focus:bg-opacity-75 g-recaptcha">
                         {{ __('Send') }}
@@ -63,7 +63,7 @@
                     </button>
                     @auth
                         @foreach($vehicles as $vehicle)
-                        <button formaction="{{ route('vin.agency.store', ['agency' => $vehicle->agency->slug]) }}" formmethod="POST" type="submit" class="relative flex items-center h-10 px-6 text-sm font-medium transition-colors rounded-full bg-m3-secondary-container text-m3-secondary-on-container dark:bg-m3-secondary-dark-container dark:text-m3-secondary-dark-on-container hover:bg-opacity-80 hover:bg-opacity-85 focus:bg-opacity-75">
+                        <button formaction="{{ route('vin.agency.store', ['agency' => $vehicle->agency->slug]) }}" formmethod="POST" type="submit" class="relative flex items-center h-10 px-6 ml-2 text-sm font-medium transition-colors rounded-full bg-m3-secondary-container text-m3-secondary-on-container dark:bg-m3-secondary-dark-container dark:text-m3-secondary-dark-on-container hover:bg-opacity-80 hover:bg-opacity-85 focus:bg-opacity-75">
                             <x-gmdi-save class="w-5 h-5 mr-2" />
                             Save to {{ $vehicle->agency->short_name }}
                         </button>
@@ -74,19 +74,19 @@
             </form>
             @endif
         </div>
-        <div class="mt-1 bg-white rounded shadow dark:bg-m3-surface-dark-variant dark:text-m3-surface-dark-on-variant">
-            <div class="flex items-center h-12 px-4 text-sm uppercase opacity-60">{{ __('Submitted suggestions') }}</div>
-            <ul>
+        <div class="py-4 mt-1 rounded-2xl bg-m3-surface-variant text-m3-surface-on-variant dark:bg-m3-surface-dark-variant dark:text-m3-surface-dark-on-variant">
+            <h2 class="text-sm font-medium leading-5 tracking-[0.007rem] px-4">{{ __('Submitted suggestions') }}</h2>
+            <ul class="font-medium">
                 @foreach($suggestions as $suggestion)
                 <li>
-                    <div class="flex items-center px-4 py-3 gap-x-4 @if($sessionVote === $suggestion->id) bg-green-100 @endif">
+                    <div class="flex items-center py-3 px-4 gap-x-4 @if($sessionVote === $suggestion->id) bg-green-100 @endif">
                         <div class="flex-grow">
-                            <p class="mb-2 leading-tight">{{ $suggestion->label }}</p>
-                            <small class="text-sm leading-tight">
-                                @if($suggestion->note)
-                                {{ $suggestion->note }} &bull;
-                                @endif
+                            <p class="leading-6">{{ $suggestion->label }}</p>
+                            <small class="text-xs leading-4">
                                 {{ $suggestion->created_at->diffForHumans() }}
+                                @if($suggestion->note)
+                                &bull; {{ $suggestion->note }}
+                                @endif
                             </small>
                             @if($sessionVote === $suggestion->id)
                                 <div class="py-0.5 px-1 rounded bg-green-500 text-white text-xs inline-block">{{ __('You have voted for this suggestion') }}</div>

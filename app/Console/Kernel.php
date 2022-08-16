f03cbb93d1b94662bdef36516c282a1ec1ccaec1
Félix Desjardins <FelixINX@users.noreply.github.com>
2022-08-16 15:27:22 -0400

    👽️ Change Schedule Monitor cleanup command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,6 +8,7 @@ use App\Jobs\RealtimeData\DispatchAgencies;
 use App\Models\Agency;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Spatie\ScheduleMonitor\Models\MonitoredScheduledTaskLogItem;
 
 class Kernel extends ConsoleKernel
 {
@@ -34,7 +35,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('backup:run')->dailyAt('02:15');
         $schedule->command('backup:monitor')->dailyAt('02:30');
         $schedule->command('schedule-monitor:sync')->dailyAt('02:45');
-        $schedule->command('schedule-monitor:clean')->dailyAt('02:50');
+        $schedule->command('model:prune', ['--model' => MonitoredScheduledTaskLogItem::class])->dailyAt('02:50');
         $schedule->command('static:update')->dailyAt('03:00');
         $schedule->command('download:clean')->dailyAt('03:55');
         $schedule->job(new CleanFolders(), 'gtfs')->dailyAt('03:55');

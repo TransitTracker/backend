3b72c03213d56520b8481cfabbee8d26a2c2a76d
Félix Desjardins <FelixINX@users.noreply.github.com>
2022-08-16 16:48:52 -0400

    🔧 Exclude stats from backups

--- config/backup.php
+++ config/backup.php
@@ -71,7 +71,11 @@ return [
              * For a complete list of available customization options, see https://github.com/spatie/db-dumper
              */
             'databases' => [
-                'mysql',
+                'mysql' => [
+                    'excludeTables' => [
+                        'stats',
+                    ],
+                ],
             ],
         ],
 

0a91b192e308e0193fa7e8d807fa80a2f492e963
Félix Desjardins <FelixINX@users.noreply.github.com>
2023-04-10 16:56:52 -0400

    🐛 Add JS (fonts) to vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/css/filament.css',
+                'resources/js/app.js',
             ],
         }),
     ],

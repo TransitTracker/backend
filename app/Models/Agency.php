9d15fd96a01484cde50777c261386d0c5fbc56ec
Félix Desjardins <FelixINX@users.noreply.github.com>
2023-09-26 09:55:37 -0400

    :bug: Add correct cast to is_archived

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -31,6 +31,7 @@ class Agency extends Model
         'is_active' => 'bool',
         'refresh_is_active' => 'bool',
         'is_exo_sector' => 'bool',
+        'is_archived' => 'bool',
         'features' => 'collection',
     ];
 

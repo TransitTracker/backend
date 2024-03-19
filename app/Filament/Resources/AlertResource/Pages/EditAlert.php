26cd092f9f82e9484ddd340d8634880a36b22293
Félix Desjardins <FelixINX@users.noreply.github.com>
2024-03-19 09:57:35 -0400

    :bug: Add LocaleSwitcher to EditAlert

--- app/Filament/Resources/AlertResource/Pages/EditAlert.php
+++ app/Filament/Resources/AlertResource/Pages/EditAlert.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\AlertResource\Pages;
 
 use App\Filament\Resources\AlertResource;
+use Filament\Actions\LocaleSwitcher;
 use Filament\Resources\Pages\EditRecord;
 use Filament\Resources\Pages\EditRecord\Concerns\Translatable;
 
@@ -11,4 +12,11 @@ class EditAlert extends EditRecord
     use Translatable;
 
     protected static string $resource = AlertResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            LocaleSwitcher::make(),
+        ];
+    }
 }
